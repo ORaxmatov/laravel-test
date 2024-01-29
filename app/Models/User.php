ded6d7d71868dffa44b8df12e32026c6b7bd3110
Oybek <raxmatovo1988@gmail.com>
2024-01-29 20:24:54 +0500

    Application model create

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,9 +48,8 @@ class User extends Authenticatable
 
 
 
-    public function role()
-    {
-        return $this->belongsTo(Role::class);
-    }
+    public function role(){return $this->belongsTo(Role::class);}
+
+    public function applications(){return $this->hasMan(Application::class);}
 
 }
