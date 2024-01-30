215ba1a365f83eba40904da3ba72add7228b0cda
Oybek <raxmatovo1988@gmail.com>
2024-01-30 20:20:39 +0500

    Add form work

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreApplicationRequest;
+use App\Models\Application;
 use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
-    //
+    public function store(StoreApplicationRequest $request)
+    {
+        if($request->hasFile('file_url')){
+            $name = $request->file('file_url')->getClientOriginalName();
+            $path = $request->file('file_url')->storeAs('files', $name, 'public');
+        }
+
+
+        $app = Application::create([
+
+            'user_id'   => auth()->user()->id,
+            'subject'   => $request->subject,
+            'message'   => $request->message,
+            'file_url'  => $path ?? null,
+
+        ]);
+
+
+        return redirect()->to('/dashboard');
+
+    }
 }
